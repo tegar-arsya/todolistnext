8036df580489a506001cc6b607cf006aeefb53c2
TEGAR ARSYADANI <tegararsya0117@gmail.com>
2024-10-23 03:22:47 +0700

    cekk

--- app/page.tsx
+++ app/page.tsx
@@ -4,11 +4,11 @@ export default function Home() {
   const currentYear = new Date().getFullYear(); // Get the current year
 
   return (
-    <div className="grid grid-rows-[auto_1fr_auto] items-center justify-items-center bg-white dark:bg-zinc-900">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+    <div className="flex flex-col min-h-screen bg-white dark:bg-zinc-900">
+      <main className="flex-grow flex flex-col gap-8 items-center sm:items-start">
         <TypewriterEffectDemo />
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center p-4 bg-gray-200 rounded-lg shadow-md">
+      <footer className="flex gap-6 flex-wrap items-center justify-center p-4 bg-gray-200 rounded-lg shadow-md">
         <p className="text-gray-600">
           © {currentYear} 
           <a 
